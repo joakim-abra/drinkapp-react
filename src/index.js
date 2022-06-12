9aef78f6ce1ba2cfa02a9a1971f9497283230416
joakim-abra <89795849+joakim-abra@users.noreply.github.com>
2022-06-12 22:54:47 +0200

    no movie

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import backgroundVideo from './shared/vid/bubl.mp4'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div id="App">
   <React.StrictMode>
     <App />
-      <div>
-<video id="background-video" autoPlay loop muted>
-<source src={backgroundVideo} type="video/mp4"/>
-</video>
-      </div>
   </React.StrictMode>
   </div>
 );
